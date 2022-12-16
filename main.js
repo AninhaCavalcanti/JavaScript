df3ad833b6b6849486e5dfc121f3e50a34e3a8a5
Ana Maria <aninha.cavalcanti.0123@gmail.com>
2022-12-15 21:08:48 -0300

    add

---
+++ main.js
@@ -0,0 +1,7 @@
+var nome = prompt("Qual o seu nome?")
+alert("É um grande prazer te conhecer, " + nome + "!")
+
+var n1 = Number(prompt("digite um número: "))
+var n2 = Number(prompt("Digite outro número: "))
+var s = n1 + n2
+alert("A soma é igual a: " + s)
